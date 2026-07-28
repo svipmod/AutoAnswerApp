31926debe930b10949e5dc2922e8a9736d3e83ee
svipmod <54392857+svipmod@users.noreply.github.com>
2026-07-29 03:13:33 +0800

    fix settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,12 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-dependencyResolution {
+
+dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
 
